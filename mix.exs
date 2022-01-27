ee2640af73a1007d16ee58d96e8e77e8f3b3e833
Tim203 <mctim203@gmail.com>
2022-01-27 14:42:54 +0100

    Bumped version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GlobalApi.MixProject do
   def project do
     [
       app: :global_api,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
